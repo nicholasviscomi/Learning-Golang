b86555d0b4fb402d62917297583882469caf1f13
Nick Viscomi <nicholasviscomi@gmail.com>
2022-03-10 12:37:29 -0500

    bitwise addition and subtraction

--- BitManipulation.go
+++ BitManipulation.go
@@ -5,5 +5,23 @@ import (
 )
 
 func main() {
-	fmt.Println(1 << 5) //2^5
+	n1 := add(3, 10)
+	fmt.Printf("%0b = %d\n", n1, n1)
+	n2 := subtract(13, 4)
+	fmt.Printf("%0b = %d\n", n2, n2)
 } 
+
+func add(x, y int) int {
+	keep := (x & y) << 1
+    res := x^y
+ 
+    if (keep == 0) {
+        return res
+	}
+
+    return add(keep, res)
+}
+
+func subtract(x, y int) int {
+	return add(x, (^y +1))
+}
